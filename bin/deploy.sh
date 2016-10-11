ad4ebed7c4fba44e926fc2baaffbaf1fa7c4156f
Paul McLanahan <pmac@mozilla.com>
2016-10-11 13:24:48 -0400

    Switch from CircleCI to Travis-CI

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,6 +3,11 @@
 # NOTE Do **not** set -x here as that would expose sensitive credentials
 #      in the Travis build logs.
 
+if [[ -z "$HEROKU_EMAIL" ]]; then
+  echo "Heroku credentials not configured"
+  exit 1
+fi
+
 DOCKER_TAG="$DOCKER_REGISTRY/$HEROKU_APP/$HEROKU_PROC_TYPE"
 echo "Pushing $DOCKER_TAG"
 docker login -u "$HEROKU_EMAIL" -p "$HEROKU_API_KEY" "$DOCKER_REGISTRY"
